61bfdc2128505c4b907f289a120715514ee15a7a
Ricardo Reyes <ricardo_reyess@hotmail.com>
2022-01-26 03:07:04 -0500

    app comunicacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { RegisterComponent } from './pages/register/register.component';
 import { ForgotpassComponent } from './pages/forgotpass/forgotpass.component';
 import { Apartment1Component } from './pages/apartment1/apartment1.component';
 import { Apartment2Component } from './pages/apartment2/apartment2.component';
+import { Comunicacion1Component } from './pages/comunicacion1/comunicacion1.component';
+import { Comunicacion2Component } from './pages/comunicacion2/comunicacion2.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,9 @@ import { Apartment2Component } from './pages/apartment2/apartment2.component';
     ForgotpassComponent,
     RegisterComponent,
     Apartment1Component,
-    Apartment2Component
+    Apartment2Component,
+    Comunicacion1Component,
+    Comunicacion2Component
   ],
   imports: [
     BrowserModule,
