0729daa3d536b1ad7505ed774569d4272058f65f
fsarangoh <82328409+fsarangoh@users.noreply.github.com>
2022-02-16 00:00:03 -0500

    SQLConection

---
+++ Backend/API_WebApplication1/Models/Apartment.cs
@@ -0,0 +1,21 @@
+namespace API_WebApplication1.Models
+{
+  public class Apartment
+  {
+    public int CodDepartamento { get; set; }
+    public string NumDepartamento { get; set; }
+
+    public string AreaDepartamento { get; set; }
+
+    public string CodCondominio{ get; set; }
+    public string NumDocPropietario { get; set; }
+    public string NumDocHabitante { get; set; }
+    public string CodSector { get; set; }
+    public string DescTipoDepart { get; set; }
+    public string NroPiso { get; set; }
+    public string NroBaños { get; set; }
+
+    public string NroHabitaciones { get; set; }
+
+  }
+}
